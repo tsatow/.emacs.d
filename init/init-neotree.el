caa7ceae05cf9742b91a0513e73cf6de28fd571e
tsatow <t.satoooooooooooo@gmail.com>
2018-09-27 20:15:46 +0900

    neotreeのProjectile向け設定を追加

--- init/init-neotree.el
+++ init/init-neotree.el
@@ -1,5 +1,21 @@
 ;; 隠しファイルをデフォルトで表示
 (setq neo-show-hidden-files t)
-;; cotrol + q でneotreeを起動
-(global-set-key (kbd "C-M-'") 'neotree-toggle)
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically.
+(setq projectile-switch-project-action 'neotree-projectile-action)
+;; Similar to find-file-in-project, NeoTree can be opened (toggled) at projectile project root.
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-dir)
+              (neotree-find file-name)))
+      (message "Could not find git project root."))))
+
+(global-set-key (kbd "C-\"") 'neotree-toggle)
+(global-set-key (kbd "C-M-'") 'neotree-project-dir)
